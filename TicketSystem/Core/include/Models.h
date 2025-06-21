cd4e69ed9936f270962b3e08aa5547a1c9437d07
LRRashkov22 <LRRashkov22@codingburgas.bg>
2025-06-21 21:23:30 +0300

    Feat structures

---
+++ TicketSystem/Core/include/Models.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+struct Movie {
+    string title;
+    string language;
+    string genre;
+    string releaseDate;
+};
+
+
+struct Show {
+    int id = 0;
+    string movieTitle;
+    string dateTime;
+    double price = 0.0;
+    string cinemaName;
+};
+
+
+struct Booking {
+    int bookingId = 0;
+    int showId = 0;
+    double finalPrice = 0.0;
+    string seatType; 
+    int hallNum = 0;
+    int seatRow = 0;
+    int seatCol = 0;
+};
